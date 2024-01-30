0ddf5aa0495b26b10a650b80d765678c78874d66
Aridane1 <aridanejdr6@gmail.com>
2024-01-30 19:45:11 +0000

    Add test of notification

---
+++ backend/src/test/notification.test.js
@@ -0,0 +1,69 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import mocha from "mocha";
+import fs from "fs";
+import path from "path";
+import app from "../app";
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe("Task Api", () => {
+  before(async () => {});
+  it("insert a new notification", (done) => {
+    chai
+      .request(app)
+      .post("/api/task")
+      .field("_id", 1)
+      .field("title", "3 dias")
+      .field("description", "Te quedan tres dias para el tiempo limite")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("title", "3 dias");
+        expect(res.body).to.have.property(
+          "description",
+          "Te quedan tres dias para el tiempo limite"
+        );
+        done();
+      });
+  });
+  it("get one notification", (done) => {
+    chai
+      .request(app)
+      .get("/api/task/1")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("title", "3 dias");
+        expect(res.body).to.have.property(
+          "description",
+          "Te quedan tres dias para el tiempo limite"
+        );
+        done();
+      });
+  });
+  it("put one notification", (done) => {
+    chai
+      .request(app)
+      .put("/api/notification/1")
+      .field("title", "1 dia")
+      .field("description", "Te quedan un dia para el tiempo limite")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property("title", "1 dias");
+        expect(res.body).to.have.property(
+          "description",
+          "Te quedan un dia para el tiempo limite"
+        );
+        done();
+      });
+  });
+  it("delete one notification", (done) => {
+    chai
+      .request(app)
+      .delete("/api/notification/1")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
