bfe5bddce8ff96e770e4b8594427bdf820f50676
Aridane1 <aridanejdr6@gmail.com>
2024-02-01 19:08:13 +0000

    hotfixed frontend

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,13 +2,7 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			colors: {
-				
-				'navbar-color': '#16282E',
-				'backgroud-color': '#FFF7EA',
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 }
