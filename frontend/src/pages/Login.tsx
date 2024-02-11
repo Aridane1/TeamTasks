4713147a9627d3c87b39939cb12343589a33f529
kirian <kirianvazquezmoreno@alumno.ieselrincon.es>
2024-02-11 17:05:31 +0000

    add desktop view for login register, nadbar and header

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -25,30 +25,64 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-8xl mt-14">Inicio</h1>
-      <img className="size-48 mt-10" src="images/TeamTaskRecortado.png" />
-      <div>
-        <form className="flex flex-col w-fit mt-16" onSubmit={handleSubmit}>
-          <label className="text-3xl my-3">Nombre de usuario</label>
-          <input
-            className="border-1 border-gray-300 rounded-full p-2 m-2 w-80 h-14"
-            type="text"
-            ref={email}
-          />
-          <label className="text-3xl my-3">Correo</label>
-          <input
-            className="border-1 border-gray-300 rounded-full p-2 m-2 w-80 h-14"
-            type="text"
-            ref={password}
-          />
-          <button
-            className="mt-20 border-2 border-gray-300 p-2 m-2 rounded-full text-4xl text-white bg-navbar"
-            type="submit"
-          >
-            Iniciar
-          </button>
-        </form>
+    <div className="h-screen">
+      <div className="flex flex-col items-center md:hidden">
+        <h1 className="text-8xl mt-14">Inicio</h1>
+        <img className="size-48 mt-10" src="images/TeamTaskRecortado.png" />
+        <div>
+          <form className="flex flex-col w-fit mt-16" onSubmit={handleSubmit}>
+            <label className="text-3xl my-3">Nombre de usuario</label>
+            <input
+              className="border-1 border-gray-300 rounded-full p-2 m-2 w-80 h-14"
+              type="text"
+              ref={email}
+            />
+            <label className="text-3xl my-3">Correo</label>
+            <input
+              className="border-1 border-gray-300 rounded-full p-2 m-2 w-80 h-14"
+              type="text"
+              ref={password}
+            />
+            <button
+              className="mt-20 border-2 border-gray-300 p-2 m-2 rounded-full text-4xl text-white bg-navbar"
+              type="submit"
+            >
+              Iniciar
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className=" hidden md:flex w-full h-full  items-center justify-between">
+        <div className="w-[50%] h-full flex items-center justify-center">
+          <img  src="images/TeamTaskRecortado.png" />
+        </div>
+        <div className="flex flex-col items-center h-full w-2/4 justify-center">
+          <h1 className="text-8xl mt-14">Inicio</h1>
+
+          <div>
+            <form className="flex flex-col w-fit mt-16" onSubmit={handleSubmit}>
+              <label className="text-3xl my-3">Nombre de usuario</label>
+              <input
+                className="border-1 border-gray-300 rounded-full p-2 m-2 w-80 h-14"
+                type="text"
+                ref={email}
+              />
+              <label className="text-3xl my-3">Correo</label>
+              <input
+                className="border-1 border-gray-300 rounded-full p-2 m-2 w-80 h-14"
+                type="text"
+                ref={password}
+              />
+              <button
+                className="mt-20 border-2 border-gray-300 p-2 m-2 rounded-full text-4xl text-white bg-navbar"
+                type="submit"
+              >
+                Iniciar
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
